a0b444a12556a00858ab1ec5bbbec2814c5020e3
nikolay <vdregdes@gmail.com>
2022-02-18 08:18:54 +0300

    загружает файлы проекта

---
+++ source/js/page-loading.js
@@ -0,0 +1,1 @@
+setTimeout(function(){document.body.classList.add('page__body--visible');}, 200);
---
+++ source/js/puzzles-homonyms.js
@@ -0,0 +1,10 @@
+let answerButton1 = document.getElementById("answer1")
+let answerButton2 = document.getElementById("answer2")
+
+answerButton1.onclick = function() {
+  answerButton1.innerHTML="Плот - Плод";
+}
+
+answerButton2.onclick = function() {
+  answerButton2.innerHTML="Лук - Луг";
+}
