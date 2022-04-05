be5d0626d146184956b7489f907bd3f5aa285f40
Nikolay <vdregdes@gmail.com>
2022-04-05 22:43:23 +0300

    gh pages

--- js/tests/test-paronyms.js
+++ js/tests/test-paronyms.js
@@ -123,52 +123,52 @@ const results =
 
 const questions =
 [
-	new Question('Синоним к слову ""',
+	new Question('Пароним к слову ""',
 	[
-		new Answer("Синоним", 0),
-		new Answer("Синоним1", 1),
-		new Answer("Синоним", 0),
-		new Answer("Синоним", 0)
+		new Answer("Пароним", 0),
+		new Answer("Пароним1", 1),
+		new Answer("Пароним", 0),
+		new Answer("Пароним", 0)
 	]),
 
-	new Question('Синоним к слову ""',
+	new Question('Пароним к слову ""',
 	[
-		new Answer("Синоним", 0),
-		new Answer("Синоним1", 1),
-		new Answer("Синоним", 0),
-		new Answer("Синоним", 0)
+		new Answer("Пароним", 0),
+		new Answer("Пароним1", 1),
+		new Answer("Пароним", 0),
+		new Answer("Пароним", 0)
 	]),
 
-	new Question('Синоним к слову ""',
+	new Question('Пароним к слову ""',
 	[
-		new Answer("Синоним", 0),
-		new Answer("Синоним", 0),
-		new Answer("Синоним", 0),
-		new Answer("Синоним1", 1)
+		new Answer("Пароним", 0),
+		new Answer("Пароним", 0),
+		new Answer("Пароним", 0),
+		new Answer("Пароним1", 1)
 	]),
 
-	new Question('Синоним к слову ""',
+	new Question('Пароним к слову ""',
 	[
-		new Answer("Синоним", 0),
-		new Answer("Синоним", 0),
-		new Answer("Синоним1", 1),
-		new Answer("Синоним", 0)
+		new Answer("Пароним", 0),
+		new Answer("Пароним", 0),
+		new Answer("Пароним1", 1),
+		new Answer("Пароним", 0)
 	]),
 
-	new Question('Синоним к слову ""',
+	new Question('Пароним к слову ""',
 	[
-		new Answer("Синоним", 0),
-		new Answer("Синоним", 0),
-		new Answer("Синоним", 0),
-		new Answer("Синоним1", 1)
+		new Answer("Пароним", 0),
+		new Answer("Пароним", 0),
+		new Answer("Пароним", 0),
+		new Answer("Пароним1", 1)
 	]),
 
-	new Question('Синоним к слову ""',
+	new Question('Пароним к слову ""',
 	[
-		new Answer("Синоним", 0),
-		new Answer("Синоним1", 1),
-		new Answer("Синоним", 0),
-		new Answer("Синоним", 0)
+		new Answer("Пароним", 0),
+		new Answer("Пароним1", 1),
+		new Answer("Пароним", 0),
+		new Answer("Пароним", 0)
 	]),
 ];
 const quiz = new Quiz(1, questions, results);
