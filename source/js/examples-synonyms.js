7fe2ff64296fe98ea4528bf9364aaf5f63a3ec34
nikolay <vdregdes@gmail.com>
2022-10-20 22:39:27 +0300

    делает показывание примеров на странице synonyms.html по кнопке

---
+++ source/js/examples-synonyms.js
@@ -0,0 +1,14 @@
+ const examples = document.querySelectorAll('.example');
+
+for (let example of examples) {
+  const buttonExample = example.querySelector('.example__button');
+
+  buttonExample.onclick = () => {
+    buttonExample.style.display = 'none';
+
+    const paragraphExamples = document.createElement('p');
+    paragraphExamples.classList.add('example__paragraph');
+    paragraphExamples.textContent = buttonExample.dataset.examples;
+    example.append(paragraphExamples);
+  }
+}
