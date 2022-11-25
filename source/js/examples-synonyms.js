91094e016aa5ad0e53af3593e2ae3fe408c90dea
nikolay <vdregdes@gmail.com>
2022-11-25 22:52:28 +0300

    добавляет новый блок с видами синонимов

--- source/js/examples-synonyms.js
+++ source/js/examples-synonyms.js
@@ -1,14 +1,14 @@
- const examples = document.querySelectorAll('.example');
+const examples = document.querySelectorAll('.example');
 
 for (let example of examples) {
   const buttonExample = example.querySelector('.example__button');
 
-  buttonExample.onclick = () => {
+  buttonExample.addEventListener('click', () => {
     buttonExample.style.display = 'none';
 
     const paragraphExamples = document.createElement('p');
     paragraphExamples.classList.add('example__paragraph');
     paragraphExamples.textContent = buttonExample.dataset.examples;
     example.append(paragraphExamples);
-  }
+  })
 }
